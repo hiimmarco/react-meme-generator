bc14962d9821c7b7977f9355c75f53a4b1201d15
Marco Bogner <marcobogner@gmail.com>
2021-09-16 17:50:51 +0200

    Add input fields, example meme and first css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import './App.css';
+import { useState } from 'react';
 import logo from './logo.svg';
 
 function App() {
+  const [toptext, setToptext] = useState('');
+  const [bottomtext, setBottomtext] = useState('');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to</p>
+        <h1>Awesome Meme Generator</h1>
+        <main className="form">
+          <label className="formlabel">
+            Text-Top:
+            <input
+              className="forminput"
+              placeholder="e.g. 'Memes'"
+              value={toptext}
+              onChange={(event) => setToptext(event.currentTarget.value)}
+            />
+          </label>
+          <label className="formlabel">
+            Bottom-Text:
+            <input
+              className="forminput"
+              placeholder="e.g. 'Memes Everywhere'"
+              value={bottomtext}
+              onChange={(event) => setBottomtext(event.currentTarget.value)}
+            />
+          </label>
+          <label>
+            Choose a car:
+            <select name="possiblememes">
+              <option value="one">One</option>
+              <option value="two">Two</option>
+              <option value="three">Three</option>
+              <option value="four">Four</option>
+            </select>
+          </label>
+          <button>Generate meme</button>
+          <p>Example:</p>
+          <img
+            src="./memes_everywhere.png"
+            alt="Placeholder Meme"
+            className="placeholderimg"
+          />
+        </main>
       </header>
     </div>
   );
