ad79e3bf1fb171df993ed0ecb40f3c14603a15a0
Marco Bogner <marcobogner@gmail.com>
2021-09-20 20:30:35 +0200

    Add dropdown with values

--- src/App.js
+++ src/App.js
@@ -2,29 +2,48 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
   const [templates, setTemplates] = useState([]);
-  const [topText, setToptext] = useState('');
-  const [bottomText, setBottomtext] = useState('');
+  const [selectedTemplate, setSelectedTemplate] = useState('SELECT A TEMPLATE');
+
+  const baseUrl = 'https://api.memegen.link/images/';
+
+  /* Get array of template information (id, name) to use in dropdown*/
 
   useEffect(() => {
-    fetch('https://api.memegen.link/templates').then((response) =>
-      response.json().then((stream) => setTemplates(stream.blank)),
-    );
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.memegen.link/templates');
+        const json = await response.json();
+        setTemplates(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
   }, []);
 
+  // Function to assign the selected template to the selectedTemplate state variable
+  const handleTemplateChange = (event) => {
+    setSelectedTemplate(event.target.value);
+  };
+
+  // Return UI elements
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to</p>
         <h1>Awesome Meme Generator</h1>
         <main className="form">
+          {/* Create text input fields for custom text */}
           <label className="formlabel">
             Text-Top:
             <input
               className="forminput"
               placeholder="e.g. 'Memes'"
               value={topText}
-              onChange={(event) => setToptext(event.currentTarget.value)}
+              onChange={(event) => setTopText(event.currentTarget.value)}
             />
           </label>
           <label className="formlabel">
@@ -33,20 +52,26 @@ function App() {
               className="forminput"
               placeholder="e.g. 'Memes Everywhere'"
               value={bottomText}
-              onChange={(event) => setBottomtext(event.currentTarget.value)}
+              onChange={(event) => setBottomText(event.currentTarget.value)}
             />
           </label>
           <label>
-            Choose a car:
-            <select name="possiblememes">
-              <option value="one">One</option>
-              <option value="two">Two</option>
-              <option value="three">Three</option>
-              <option value="four">Four</option>
+            Choose a template:
+            <select onChange={handleTemplateChange}>
+              <option value="SELECT A TEMPLATE">
+                {' '}
+                -- Select a template --{' '}
+              </option>
+              {templates.map((meme) => (
+                <option key={meme.id} value={meme.id}>
+                  {meme.name}
+                </option>
+              ))}
             </select>
           </label>
+
           <button>Generate meme</button>
-          <p>Example:</p>
+          <p>Your generated meme:</p>
           <img
             src="./memes_everywhere.png"
             alt="Example Meme"
@@ -59,3 +84,11 @@ function App() {
 }
 
 export default App;
+
+/*
+{templates.map((item) => (
+  <option value={item.id} key={item.id}>
+    {item.name}
+  </option>
+))}
+*/
